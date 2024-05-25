be7d496418b6cf4e75a0356177b8d5992122cd8e
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-05-25 17:06:17 +0200

    Creata app Accounts. Collegata homepage con login form.

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def index(request):
     return render(request, 'store/index.html')
+
+def goto_login(request):
+    return redirect('accounts:login')
