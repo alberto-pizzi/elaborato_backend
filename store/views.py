4512ceb47cb344f3db16759408af21e787e453dc
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-05-26 03:16:26 +0200

    Iniziato a riempire il database con alcuni esempi. Store collegato al database (da ottimizzare). Aggiunti modelli per db.

--- store/views.py
+++ store/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from .models import Product
 
 def index(request):
-    return render(request, 'store/index.html')
+
+    products = Product.objects.all().values('name', 'price')
+
+    return render(request, 'store/index.html', {'products': products})
 
 def goto_login(request):
     return redirect('accounts:login')
