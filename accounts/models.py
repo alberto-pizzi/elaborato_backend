50beb030775efe38fadff6146948ecdc5a10d51e
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-05-27 14:15:08 +0200

    Corretto form signup. Iniziato a creare modello profili per db

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    address1 = models.CharField(max_length=255, blank=False)
+    address2 = models.CharField(max_length=255, blank=True)
+    country = models.CharField(max_length=255, blank=False)
+    state = models.CharField(max_length=255, blank=False)
+    zip = models.CharField(max_length=20, blank=False)
+    def __str__(self):
+        return self.user.username
