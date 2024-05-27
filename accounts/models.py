d0363da5b9469563564784d89e64f0d8ea9de6cf
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-05-28 01:18:09 +0200

    Implementato CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=30, blank=False)
-    last_name = models.CharField(max_length=30, blank=False)
-    address1 = models.CharField(max_length=255, blank=False)
-    address2 = models.CharField(max_length=255, blank=True)
-    country = models.CharField(max_length=255, blank=False)
-    state = models.CharField(max_length=255, blank=False)
-    zip = models.CharField(max_length=20, blank=False)
+
+class CustomUser(AbstractUser):
+    address1 = models.CharField(max_length=255, blank=False, null=False)
+    address2 = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=False, null=False)
+    state = models.CharField(max_length=255, blank=False, null=False)
+    zip = models.CharField(max_length=20, blank=False, null=False)
     def __str__(self):
-        return self.user.username
+        return self.username
--- store/models.py
+++ store/models.py
@@ -11,9 +11,9 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False,blank=False)
     date_added = models.DateTimeField(auto_now_add=True, null=True)
-    stock = models.IntegerField(default=0, null=False,blank=True)
-    purchased = models.IntegerField(default=0, null=False,blank=True)
-    categories = models.ManyToManyField(Category, related_name='products', null=True, blank=True)
+    stock = models.PositiveIntegerField(default=0, null=False,blank=True)
+    purchased = models.PositiveIntegerField(default=0, null=False,blank=True)
+    categories = models.ManyToManyField(Category, null=True, blank=True)
     def __str__(self):
         return self.name
 
