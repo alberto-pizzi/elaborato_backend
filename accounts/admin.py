732e92d1a8b3f8b07a72a713b01ad854f39a5261
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-06-19 21:14:56 +0200

    Migliorato pannello admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,5 +3,34 @@ from django.contrib import admin
 # Register your models here.
 from .models import CustomUser,Address
 
-admin.site.register(CustomUser)
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group
+
+
+class CustomUserAdmin(DefaultUserAdmin):
+    #add_form = UserCreationForm
+
+    fieldsets = (
+        (None, {'fields': ('username',)}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',)})
+    )
+    readonly_fields = ('id','date_joined')
+
+    list_display = ('id','username','email','first_name','last_name','is_staff','is_active','is_superuser','date_joined')
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password', 'email', 'first_name', 'last_name','is_active', 'is_staff', 'is_superuser',),
+        }),
+    )
+
+admin.site.register(CustomUser,CustomUserAdmin)
+
 admin.site.register(Address)
+admin.site.unregister(Group)
+
+
